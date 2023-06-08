c42cb5a5f54cb73e97d2523f238727fa06801178
v-anjuvs <v-anjuvs@microsoft.com>
2023-06-08 17:14:59 +0530

    removed testServer

--- playwright/utils/fetchTestPageUrl.ts
+++ playwright/utils/fetchTestPageUrl.ts
@@ -2,7 +2,8 @@ import {join} from 'path';
 
 const fetchTestPageUrl = (): string => {
     const testPage = join('file:', __dirname, '..', 'public', 'index.html');
-    return  process.env?.testServer || testPage;
+    // return  process.env?.testServer || testPage;
+    return testPage;
 }
 
 export default fetchTestPageUrl;
